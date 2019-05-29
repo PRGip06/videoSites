7ddab501e6ee3420298f0a9db21b78b25f9cf0c4
Ricardo <rgipson1@gmail.com>
2019-05-29 12:26:06 -0500

    add videoSite.sql file

---
+++ videoSite.sql
@@ -0,0 +1,334 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 28, 2019 at 10:08 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.2.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `videoSite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Film & Animation'),
+(2, 'Autos & Vehicles'),
+(3, 'Music'),
+(4, 'Pets & Animals'),
+(5, 'Sports'),
+(6, 'Travel & Events'),
+(7, 'Gaming'),
+(8, 'People & Blogs'),
+(9, 'Comedy'),
+(10, 'Entertainment'),
+(11, 'News & Politics'),
+(12, 'HowTo & Style'),
+(13, 'Education'),
+(14, 'Science & Technology'),
+(15, 'Nonprofits & Activism');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `postedBy` varchar(50) NOT NULL,
+  `videoid` int(11) NOT NULL,
+  `responseTo` int(11) NOT NULL,
+  `body` text NOT NULL,
+  `datePosted` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `postedBy`, `videoid`, `responseTo`, `body`, `datePosted`) VALUES
+(79, 'zp-Penn', 59, 0, 'What a majestic video', '2019-05-28 19:33:43'),
+(80, 'jg-Gates', 61, 0, 'This is so awesome!!!', '2019-05-28 19:41:01'),
+(81, 'jg-Gates', 61, 80, 'Paradise at its best', '2019-05-28 19:41:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dislikes`
+--
+
+CREATE TABLE `dislikes` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `commentid` int(11) NOT NULL,
+  `videoid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `commentid` int(11) NOT NULL,
+  `videoid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `username`, `commentid`, `videoid`) VALUES
+(104, 'zp-Penn', 79, 0),
+(105, 'zp-Penn', 0, 59),
+(106, 'jg-Gates', 0, 60),
+(107, 'jg-Gates', 0, 61),
+(108, 'jg-Gates', 80, 0),
+(109, 'Joe-Joe', 80, 0),
+(110, 'Joe-Joe', 0, 61);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int(11) NOT NULL,
+  `userTo` varchar(50) NOT NULL,
+  `userFrom` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subscribers`
+--
+
+INSERT INTO `subscribers` (`id`, `userTo`, `userFrom`) VALUES
+(34, 'jg-Gates', 'zp-Penn'),
+(35, 'zp-Penn', 'jg-Gates'),
+(36, 'jg-Gates', 'Joe-Joe');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thumbnails`
+--
+
+CREATE TABLE `thumbnails` (
+  `id` int(11) NOT NULL,
+  `videoid` int(11) NOT NULL,
+  `filePath` varchar(250) NOT NULL,
+  `selected` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `thumbnails`
+--
+
+INSERT INTO `thumbnails` (`id`, `videoid`, `filePath`, `selected`) VALUES
+(109, 59, 'uploads/videos/thumbnails/videoId-5ced8cbff241c.jpg', 1),
+(110, 59, 'uploads/videos/thumbnails/videoId-5ced8cc17b4a4.jpg', 0),
+(111, 59, 'uploads/videos/thumbnails/videoId-5ced8cc488704.jpg', 0),
+(112, 60, 'uploads/videos/thumbnails/videoId-5ced8dcdd71c5.jpg', 1),
+(113, 60, 'uploads/videos/thumbnails/videoId-5ced8dcea6c07.jpg', 0),
+(114, 60, 'uploads/videos/thumbnails/videoId-5ced8dd03584a.jpg', 0),
+(115, 61, 'uploads/videos/thumbnails/videoId-5ced8e9fcd59a.jpg', 1),
+(116, 61, 'uploads/videos/thumbnails/videoId-5ced8ea0c036b.jpg', 0),
+(117, 61, 'uploads/videos/thumbnails/videoId-5ced8ea2897e9.jpg', 0),
+(118, 62, 'uploads/videos/thumbnails/videoId-5ced8fc808402.jpg', 1),
+(119, 62, 'uploads/videos/thumbnails/videoId-5ced8fc88235a.jpg', 0),
+(120, 62, 'uploads/videos/thumbnails/videoId-5ced8fc970090.jpg', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(25) NOT NULL,
+  `lastName` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `signUpDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `profilePic` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `username`, `email`, `password`, `signUpDate`, `profilePic`) VALUES
+(4, 'Bobby', 'Gates', 'jg-Gates', 'Bg@gates.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', '2019-04-06 09:37:33', 'assets/images/profilePictures/default-male.png'),
+(5, 'Joe', 'Joe', 'Joe-Joe', 'JoeJoe@joe.com', 'b109f3bbbc244eb82441917ed06d618b9008dd09b3befd1b5e07394c706a8bb980b1d7785e5976ec049b46df5f1326af5a2ea6d103fd07c95385ffab0cacbc86', '2019-04-08 06:00:51', 'assets/images/profilePictures/default.png'),
+(6, 'Zuri', 'Penn', 'zp-Penn', 'zp@Penn.com', 'bc547750b92797f955b36112cc9bdd5cddf7d0862151d03a167ada8995aa24a9ad24610b36a68bc02da24141ee51670aea13ed6469099a4453f335cb239db5da', '2019-04-10 19:03:53', 'assets/images/profilePictures/default-female.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `uploadedBy` varchar(50) NOT NULL,
+  `title` varchar(70) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `privacy` int(11) NOT NULL,
+  `filePath` varchar(250) NOT NULL,
+  `category` int(11) NOT NULL,
+  `uploadDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `views` int(11) NOT NULL,
+  `duration` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`id`, `uploadedBy`, `title`, `description`, `privacy`, `filePath`, `category`, `uploadDate`, `views`, `duration`) VALUES
+(59, 'zp-Penn', 'Beach Lighthouse', 'A lighthouse overlooking the ocean', 1, 'uploads/videos/5ced8c65d62c1.mp4', 6, '2019-05-28 14:30:45', 3, '00:21'),
+(60, 'jg-Gates', 'Ocean Waves', 'Crashing waves as the tide comes in', 1, 'uploads/videos/5ced8da3d4cbd.mp4', 6, '2019-05-28 14:36:03', 1, '00:08'),
+(61, 'jg-Gates', 'Orange Phoenix Sky', 'An orange sky is blankets the ocean', 1, 'uploads/videos/5ced8e6c8ab40.mp4', 6, '2019-05-28 14:39:24', 5, '00:11'),
+(62, 'jg-Gates', 'The Caribbean Life', 'A boat tour of the beach in the caribbean', 1, 'uploads/videos/5ced8fa9eacf0.mp4', 6, '2019-05-28 14:44:41', 0, '00:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `thumbnails`
+--
+ALTER TABLE `thumbnails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=82;
+
+--
+-- AUTO_INCREMENT for table `dislikes`
+--
+ALTER TABLE `dislikes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `thumbnails`
+--
+ALTER TABLE `thumbnails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=121;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
