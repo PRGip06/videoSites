a14f0e7e887063fb082c65964f081f56d2a8742e
Ricardo <rgipson1@gmail.com>
2019-05-30 11:42:15 -0500

    update config file

--- includes/config.php
+++ includes/config.php
@@ -9,6 +9,7 @@ date_default_timezone_set("America/Chicago");
 //Connecting to the database. ("mysql:dbname=videoSite;host=localhost", "root", "");
 try {
 		$con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+		echo "connection made";
 		$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
 catch (PDOException $e) {
